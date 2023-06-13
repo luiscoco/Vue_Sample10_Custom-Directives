b524f5d1302f06555dd175d2ddf60af6a059a1de
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-06-13 10:58:42 +0200

    second-commit

--- src/directives/customDirective.ts
+++ src/directives/customDirective.ts
@@ -5,13 +5,16 @@ const customDirective = {
     // Directive logic when element is mounted
     // Access the binding value using binding.value
     el.textContent = binding.value;
+    console.log("Message from the mounted hook");
   },
   updated(el: HTMLElement, binding: DirectiveBinding<string>) {
     // Directive logic when element is updated
     el.textContent = binding.value;
+    console.log("Message from the updated hook");
   },
   unmounted(el: HTMLElement) {
     // Directive logic when element is unmounted
+    console.log("Message from the unmounted hook");
   },
 };
 
